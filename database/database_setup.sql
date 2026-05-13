7dfa3ff9581f628b6281d6f7b0762a158d58ac88
Hassanah-dev006 <harseenarhb18@gmail.com>
2026-05-13 16:11:13 +0200

    Database setup

---
+++ database/database_setup.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS system_logs;
+DROP TABLE IF EXISTS transaction_tags;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS transaction_categories;
+DROP TABLE IF EXISTS users;
